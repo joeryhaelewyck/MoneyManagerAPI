42eef333899a55c5e91b90e4396e783b9e841550
Joery Haelewyck <joery.haelewyck@tui.be>
2020-05-12 13:40:10 +0200

    add interface with crud operations for costRepository

---
+++ MoneyManagerApi/Models/ICostRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyManagerApi.Models
+{
+    interface ICostRepository
+    {
+        Cost getById(int id);
+        Cost getByName(string name);
+        IEnumerable<Cost> getByType(string type);
+        IEnumerable<Cost> getAll();
+        void Add(Cost cost);
+        void Delete(Cost cost);
+        void Update(Cost cost);
+        void SaveChanges();
+    }
+}
