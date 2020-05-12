bce2a9e4e3af6422fda5bf156b62c78106f24380
Joery Haelewyck <joery.haelewyck@tui.be>
2020-05-12 15:50:51 +0200

    separate configuration for costs from dbcontext

---
+++ MoneyManagerApi/Data/Mappers/CostConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyManagerApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyManagerApi.Data.Mappers
+{
+    public class CostConfiguration : IEntityTypeConfiguration<Cost>
+    {
+        public void Configure(EntityTypeBuilder<Cost> builder)
+        {
+            //table name
+            builder.ToTable("Costs");
+            //Primary key
+            builder.HasKey(C => C.Id);
+            //Properties
+            builder.Property(C => C.Name).HasColumnName("NAME").IsRequired();
+            builder.Property(C => C.Type).HasColumnName("TYPE").IsRequired();
+            builder.Property(C => C.Amount).HasColumnName("Amount").IsRequired();
+        }
+    }
+}
--- MoneyManagerApi/Data/TransactionContext.cs
+++ MoneyManagerApi/Data/TransactionContext.cs
@@ -1,31 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MoneyManagerApi.Data.Mappers;
 using MoneyManagerApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MoneyManagerApi.Data
 {
     public class TransactionContext : DbContext
     {
-        public TransactionContext(DbContextOptions<TransactionContext> options)
-            : base(options) 
+        public TransactionContext(DbContextOptions<TransactionContext> options) : base(options) 
         { 
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Cost>()
-                .Property(c => c.Name).IsRequired().HasMaxLength(50);
-            modelBuilder.Entity<Cost>()
-                .Property(c => c.Type).IsRequired().HasMaxLength(50);
-            modelBuilder.Entity<Cost>()
-                .Property(c => c.Amount).IsRequired().HasMaxLength(50);
+            modelBuilder.ApplyConfiguration(new CostConfiguration());
 
             modelBuilder.Entity<Cost>().HasData(
-                   new Cost { Id = 1, Name = "Dans Lessen", Type = "Maandelijks", Amount = 50},
-                   new Cost { Id = 2, Name = "netflix", Type = "Maandelijks", Amount = 12},
+                   new Cost { Id = 1, Name = "DansLessen", Type = "Maandelijks", Amount = 50m},
+                   new Cost { Id = 2, Name = "netflix", Type = "Maandelijks", Amount = 12m},
                    new Cost { Id = 3, Name = "booschappn", Type = "eenmalig", Amount = 69.60m }
                 );
         }
