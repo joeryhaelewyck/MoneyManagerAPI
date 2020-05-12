99a65bab7cc9cc97fbd42fed14b12cb9004f6a85
Joery Haelewyck <joery.haelewyck@tui.be>
2020-05-12 13:51:38 +0200

    add basic CostRepository

---
+++ MoneyManagerApi/Data/Repositories/CostRepository.cs
@@ -0,0 +1,51 @@
+﻿using MoneyManagerApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyManagerApi.Data.Repositories
+{
+    public class CostRepository : ICostRepository
+    {
+        public void Add(Cost cost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Cost cost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cost> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cost getById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cost getByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cost> getByType(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Cost cost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
