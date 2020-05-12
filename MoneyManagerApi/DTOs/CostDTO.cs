510765e6cdd74e3de2c15dda43d0534c79c5b092
Joery Haelewyck <joery.haelewyck@tui.be>
2020-05-12 16:43:16 +0200

    add DTO for cost in order to add costs

---
+++ MoneyManagerApi/DTOs/CostDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyManagerApi.DTOs
+{
+    public class CostDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
