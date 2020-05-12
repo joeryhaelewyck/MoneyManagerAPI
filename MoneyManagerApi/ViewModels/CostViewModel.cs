d7a82c865f80f5f11bb718975ffcb58eaefe444e
Joery Haelewyck <joery.haelewyck@tui.be>
2020-05-12 20:26:23 +0200

    add viewmodel for cost

---
+++ MoneyManagerApi/ViewModels/CostViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyManagerApi.ViewModels
+{
+    public class CostViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Frequency { get; set; }
+        public decimal Amount {get ; set;}
+    }
+}
