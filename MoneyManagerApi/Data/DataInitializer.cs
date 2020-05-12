fbcf589259e8868f0110ba6913d8ba48f7b96b40
Joery Haelewyck <joery.haelewyck@tui.be>
2020-05-12 14:56:49 +0200

    add data initializer to fill up database

---
+++ MoneyManagerApi/Data/DataInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyManagerApi.Data
+{
+   
+    public class DataInitializer
+    {
+        private readonly TransactionContext _context;
+        public DataInitializer(TransactionContext context)
+        {
+            _context = context;
+        }
+        public void InitializeData()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+        }
+    }
+}
