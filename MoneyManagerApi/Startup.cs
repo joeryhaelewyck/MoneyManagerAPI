739f54f0a4274e4833f7259ee21164cfbb4e90c1
Joery Haelewyck <joery.haelewyck@tui.be>
2020-05-14 15:53:22 +0200

    add configuration for JWT token in startup + adjust swagger for authorize

--- MoneyManagerApi/Startup.cs
+++ MoneyManagerApi/Startup.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MoneyManagerApi.Data;
 using MoneyManagerApi.Data.Repositories;
 using MoneyManagerApi.Data.Repositories.Contracts;
 using MoneyManagerApi.Mappers;
+using MoneyManagerApi.Models;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenApiSecurityRequirement = NSwag.OpenApiSecurityRequirement;
+using OpenApiSecurityScheme = NSwag.OpenApiSecurityScheme;
 
 namespace MoneyManagerApi
 {
@@ -23,6 +36,10 @@ namespace MoneyManagerApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var jwtSettings = new JwtSettings();
+            Configuration.Bind(nameof(jwtSettings), jwtSettings);
+            services.AddSingleton(jwtSettings);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<TransactionContext>(opt =>
@@ -34,12 +51,55 @@ namespace MoneyManagerApi
             services.AddOpenApiDocument(c =>
             {
                 c.DocumentName = "apidocs";
-                c.Title = "RecipeAPI";
+                c.Title = "TransactionAPI";
                 c.Version = "v1";
                 c.Description = "The Transaction documentation description.";
+                c.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Description = "JWT Authorization header using the bearer scheme"
+                });
+                c.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+
             });
             services.AddCors(options => options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
-        }
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters =
+                    new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true
+                    };
+            })
+            .AddCookie(options =>
+            {
+                options.Events =
+                new CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = context =>
+                    {
+                        var loginPath = string.Concat("https://", context.Request.Host, "/Login");
+                        context.HttpContext.Response.Redirect(loginPath);
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, DataInitializer dataInitializer)
@@ -59,7 +119,7 @@ namespace MoneyManagerApi
             app.UseOpenApi();
             app.UseSwaggerUi3();
             app.UseCors("AllowAllOrigins");
-
+            app.UseAuthentication();
 
             dataInitializer.InitializeData();
 
